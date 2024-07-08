65695783ca7851610c0f325c0d2ef35a74caee21
unknown <marlinzeah@gmail.com>
2024-07-08 17:55:45 -0400

    British GP

--- src/data/F1/2024/raceResults2024.ts
+++ src/data/F1/2024/raceResults2024.ts
@@ -180,12 +180,16 @@ export const FormulaOneRaceResults : F1RaceType[] = [
   },
   {
     location: "Great Britain",
-    finalOrder: [],
-    DNFs: [],
+    finalOrder: [
+      "HAM", "VER", "NOR", "PIA", "SAI", "HUL", "STR", "ALO", "ALB", "TSU", "SAR", "MAG", "RIC", "LEC", "BOT", "OCO", "PER", "ZHO"
+    ],
+    DNFs: [
+      "RUS", "GAS"
+    ],
     fastestLap: "VER",
     polePosition: "VER",
     sprint: false,
-    completed: false,
+    completed: true,
   },
   {
     location: "Hungary",

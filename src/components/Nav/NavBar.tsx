dd88a6e4671cf646969dd21d1efe7777345c9e6e
unknown <marlinzeah@gmail.com>
2024-09-11 23:45:55 -0400

    Nav MLS added

--- src/components/Nav/NavBar.tsx
+++ src/components/Nav/NavBar.tsx
@@ -145,6 +145,17 @@ const NavBar: React.FC<NavProps> = (props: NavProps) => {
             </MenubarSubContent>
           </MenubarSub>
           <MenubarSub>
+            <MenubarSubTrigger>MLS</MenubarSubTrigger>
+            <MenubarSubContent>
+              <MenubarItem asChild>
+                <Link href="/mls/finals">Finals</Link>
+              </MenubarItem>
+              <MenubarItem asChild>
+                <Link href="/mls/rank">Rank</Link>
+              </MenubarItem>
+            </MenubarSubContent>
+          </MenubarSub>
+          <MenubarSub>
             <MenubarSubTrigger>WNBA</MenubarSubTrigger>
             <MenubarSubContent>
               <MenubarItem asChild>

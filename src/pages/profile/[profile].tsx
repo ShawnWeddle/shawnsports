54a445f7fa854ef11c1fc54356137de8f98e6778
unknown <marlinzeah@gmail.com>
2026-08-12 23:50:20 -0400

    Profile page needs overhaul

--- src/pages/profile/[profile].tsx
+++ src/pages/profile/[profile].tsx
@@ -1,16 +1,13 @@
 import { useState, useEffect } from "react";
 import { useRouter } from "next/router";
-import { useAuthContext } from "~/hooks/useAuthContext";
 import MainPage from "~/components/Page/MainPage";
 import NoteFromDev from "~/components/Home/NoteFromDev";
-import UserProfile from "~/components/profile/TeamInputs";
-import MainProfile from "~/components/profile/MainProfile";
+// import UserProfile from "~/components/profile/TeamInputs";
+// import MainProfile from "~/components/profile/MainProfile";
 
 const ProfilePage = () => {
   const router = useRouter();
   const [pageUsername, setPageUsername] = useState<string>();
-  const { authState, authDispatch } = useAuthContext();
-  const user = authState.user;
 
   useEffect(() => {
     const routerQueryUsername = router.query.profile;
